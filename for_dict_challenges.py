8316e9acd4f93d4cc60e4db8ac38e73ea5dda326
Шестопалов Артём Викторович <artem.shestopalov@tele2.ru>
2019-03-14 17:53:30 +0300

    добавил задание 4

--- for_dict_challenges.py
+++ for_dict_challenges.py
@@ -96,31 +96,38 @@ def count_repeat(school_students):
 for check_class in school_students:
   pop_name = count_repeat(check_class)
   max_repeat = 0
-  clas_num += 1
   for name_student, num_repeat in pop_name.items():
       if num_repeat > max_repeat:
           max_repeat = num_repeat
           max_repeat_student = name_student
-  print(f'Самое частое имя в {clas_num} классе:{max_repeat_student}')
-
+  print(f'Самое частое имя в {clas_num + 1} классе:{max_repeat_student}')
 
 # Задание 4
 # Для каждого класса нужно вывести количество девочек и мальчиков в нём.
-school = [
-  {'class': '2a', 'students': [{'first_name': 'Маша'}, {'first_name': 'Оля'}]},
-  {'class': '3c', 'students': [{'first_name': 'Олег'}, {'first_name': 'Миша'}]},
-]
+school = [{'class': '2a', 'students': [{'first_name': 'Маша'}, {'first_name': 'Оля'}]},
+          {'class': '3c', 'students': [{'first_name': 'Олег'}, {'first_name': 'Миша'}]},
+          ]
 is_male = {
   'Маша': False,
   'Оля': False,
   'Олег': True,
   'Миша': True,
 }
-# ???
 
 # Пример вывода:
 # В классе 2a 2 девочки и 0 мальчика.
 # В классе 3c 0 девочки и 2 мальчика.
+
+for class_list in school:
+  man = 0
+  not_man = 0
+  for student_name in class_list['students']:
+    name = student_name['first_name']
+    if name in is_male and is_male[name] == False:
+      not_man += 1
+    if name in is_male and is_male[name] == True:
+      man += 1
+  print(f"В классе {class_list['class']} {not_man} девочки и {man} мальчиков")
 
 
 # Задание 5
--- test.py
+++ test.py
@@ -1,38 +1,27 @@
-# Задание 3
-# Есть список учеников в нескольких классах, нужно вывести самое частое имя в каждом классе.
-school_students = [
-  [  # это – первый класс
-    {'first_name': 'Вася'},
-    {'first_name': 'Вася'},
-  ],
-  [  # это – второй класс
-    {'first_name': 'Маша'},
-    {'first_name': 'Маша'},
-    {'first_name': 'Оля'},
-  ]
-]
+# Задание 4
+# Для каждого класса нужно вывести количество девочек и мальчиков в нём.
+school = [{'class': '2a', 'students': [{'first_name': 'Маша'}, {'first_name': 'Оля'}]},
+          {'class': '3c', 'students': [{'first_name': 'Олег'}, {'first_name': 'Миша'}]},
+          ]
+is_male = {
+  'Маша': False,
+  'Оля': False,
+  'Олег': True,
+  'Миша': True,
+}
 
 # Пример вывода:
-# Самое частое имя в классе 1: Вася
-# Самое частое имя в классе 2: Маша
+# В классе 2a 2 девочки и 0 мальчика.
+# В классе 3c 0 девочки и 2 мальчика.
 
-clas_num = 0
+for class_list in school:
+  man = 0
+  not_man = 0
+  for student_name in class_list['students']:
+    name = student_name['first_name']
+    if name in is_male and is_male[name] == False:
+      not_man += 1
+    if name in is_male and is_male[name] == True:
+      man += 1
+  print(f"В классе {class_list['class']} {not_man} девочки и {man} мальчиков")
 
-def count_repeat(school_students):
-  dict_result = {}
-  for student_data in check_class:
-    if student_data['first_name'] in dict_result:
-      dict_result[student_data['first_name']] += 1
-    else:
-      dict_result[student_data['first_name']] = 1
-  return dict_result
-
-for check_class in school_students:
-  pop_name = count_repeat(check_class)
-  max_repeat = 0
-  clas_num += 1
-  for name_student, num_repeat in pop_name.items():
-      if num_repeat > max_repeat:
-          max_repeat = num_repeat
-          max_repeat_student = name_student
-  print(f'Самое частое имя в {clas_num} классе:{max_repeat_student}')
