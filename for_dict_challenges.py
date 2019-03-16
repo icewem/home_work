2283204457f4b28830222f5d1e725148666082a8
Artem Shestopalov <ArtShe@MacBook-Pro-Artem.local>
2019-03-16 22:19:42 +0300

    доделал 5 задание

--- for_dict_challenges.py
+++ for_dict_challenges.py
@@ -146,3 +146,32 @@ is_male = {
 # Пример вывода:
 # Больше всего мальчиков в классе 3c
 # Больше всего девочек в классе 2a
+
+max_male_class = {
+  'class': '',
+  'num_male': 0
+}
+
+max_female_class = {
+  'class': '',
+  'num_female': 0
+}
+
+
+for class_info in school:
+  male = 0
+  female = 0
+  for student_name in class_info['students']:
+    name = student_name['first_name']
+    if name in is_male and is_male[name] == False:
+      female += 1
+    if name in is_male and is_male[name] == True:
+      male += 1
+  if female > max_female_class['num_female']:
+    max_female_class['class'] = class_info['class']
+    max_female_class['num_female'] = female
+  if male > max_male_class['num_male']:
+    max_male_class['class'] = class_info['class']
+    max_male_class['num_male'] = male
+print(f'Больше всего мальчиков в классе {max_male_class["class"]}')
+print(f'Больше всего девочек в классе {max_female_class["class"]}')
--- test.py
+++ test.py
@@ -1,18 +1,31 @@
-# Задание 4
-# Для каждого класса нужно вывести количество девочек и мальчиков в нём.
-school = [{'class': '2a', 'students': [{'first_name': 'Маша'}, {'first_name': 'Оля'}]},
-          {'class': '3c', 'students': [{'first_name': 'Олег'}, {'first_name': 'Миша'}]},
-          ]
+# Задание 5
+# По информации о учениках разных классов нужно найти класс, в котором больше всего девочек и больше всего мальчиков.
+school = [
+  {'class': '2a', 'students': [{'first_name': 'Маша'}, {'first_name': 'Оля'}]},
+  {'class': '3c', 'students': [{'first_name': 'Олег'}, {'first_name': 'Миша'}]},
+]
 is_male = {
   'Маша': False,
   'Оля': False,
   'Олег': True,
   'Миша': True,
 }
+# ???
 
 # Пример вывода:
-# В классе 2a 2 девочки и 0 мальчика.
-# В классе 3c 0 девочки и 2 мальчика.
+# Больше всего мальчиков в классе 3c
+# Больше всего девочек в классе 2a
+
+max_male_class = {
+  'class': '',
+  'num_male': 0
+}
+
+max_female_class = {
+  'class': '',
+  'num_female': 0
+}
+
 
 for class_info in school:
   male = 0
@@ -23,5 +36,11 @@ for class_info in school:
       female += 1
     if name in is_male and is_male[name] == True:
       male += 1
-  print(f"В классе {class_info['class']} {female} девочки и {male} мальчиков")
-
+  if female > max_female_class['num_female']:
+    max_female_class['class'] = class_info['class']
+    max_female_class['num_female'] = female
+  if male > max_male_class['num_male']:
+    max_male_class['class'] = class_info['class']
+    max_male_class['num_male'] = male
+print(f'Больше всего мальчиков в классе {max_male_class["class"]}')
+print(f'Больше всего девочек в классе {max_female_class["class"]}')
